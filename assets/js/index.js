c6077d0afa456bfc578f4694dff1d30e7b7a4f9f
chris097 <chrisfidel.international@gmail.com>
2020-06-17 13:08:26 +0100

    update js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,50 +5,54 @@
 */
 
     // 1. UIController gonna be our helper
-//     function UIController(className, value){
-//         let UI = document.querySelector(className);
-//         if(value){
-//           UI.style.display = value;
-//         }
-//         return UI;
-//       };
+     function UIController(className, value){
+         let UI = document.querySelector(className);
+         if(value){
+           UI.style.display = value;
+         }
+         return UI;
+      };
  
-//  // 2. store classes && values in our object
-//  function createUIClass(){
-//      return {
-//        getClass : function(){
-//          return{
-//             block: 'block',
-//             none: 'none',
-//             menuBurgerA: '.menu-burgerA',
-//             menuBurgerB: '.menu-burgerB',
-//             click: 'click',
-//          }
-//        }
-//      }
-//    };
+  // 2. store classes && values in our object
+  function createUIClass(){
+      return {
+        getClass : function(){
+          return{
+             block: 'block',
+             none: 'none',
+             menuBurgerA: '.menu-burgerA ',
+             menuBurgerB: '.menu-burgerB img',
+             click: 'click',
+          }
+        }
+      }
+   };
  
-//    function eventController (UIClass){
-//     // 3-1. global variables
-//     let inputClass, inputBtn;
-//      inputClass = UIClass();
-//      inputBtn = inputClass.getClass();
+    function eventController (UIClass){
+     // 3-1. global variables
+     let inputClass, inputBtn;
+      inputClass = UIClass();
+      inputBtn = inputClass.getClass();
 
-//     const displayMenuBurgerA = () =>{ 
-//         UIController(inputBtn.menuBurgerA, inputBtn.none); 
-//         UIController(inputBtn.menuBurgerB, inputBtn.block);
-//     };
+     const displayMenuBurgerA = () =>{ 
+         UIController(inputBtn.menuBurgerA, inputBtn.none); 
+         UIController(inputBtn.menuBurgerB, inputBtn.block);
+     };
 
-//     const displayMenuBurgerB = () =>{
-//         UIController(inputBtn.menuBurgerB, inputBtn.none); 
-//         UIController(inputBtn.menuBurgerA, inputBtn.block); 
-//     };
+     const displayMenuBurgerB = () =>{
+         UIController(inputBtn.menuBurgerB, inputBtn.none); 
+         UIController(inputBtn.menuBurgerA, inputBtn.block); 
+     };
 
-//     UIController(inputBtn.menuBurgerA).addEventListener(inputBtn.click, displayMenuBurgerA);
-//     UIController(inputBtn.menuBurgerB).addEventListener(inputBtn.click, displayMenuBurgerB);
-// }
-// eventController(createUIClass)
+     UIController(inputBtn.menuBurgerA).addEventListener(inputBtn.click, displayMenuBurgerA);
+     UIController(inputBtn.menuBurgerB).addEventListener(inputBtn.click, displayMenuBurgerB);
+ }
+eventController(createUIClass)
 
-document.querySelector('.menu-burgerA').addEventListener('click', () =>{
-   document.querySelector('.menu-burgerB').display.style = 'block'
-})
+// document.querySelector('.menu-burgerA').addEventListener('click', () =>{
+//   //  const res =document.querySelector('.menu-burgerA').style.display = 'none'
+//    const res1 =document.querySelector('.mobile-navbar .menu-burgerB img').style.display = 'block';
+//    const res =document.querySelector('.mobile-navbar .menu-burgerA img').style.display = 'none';
+//    console.log(res)
+//    console.log(res1)
+// })
